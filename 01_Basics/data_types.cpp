bfd5a418917c5a589a8144ae355464e75230162b
Atharav Gupta <atharavgupta4519@gmail.com>
2026-07-11 15:57:50 +0530

    Added C++ basics programs

---
+++ 01_Basics/data_types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 14;
+    float marks = 45.76;
+    char grade = 'A';
+    bool isStudent = true;
+    cout << "Age: " << age;
+    cout << "\nMarks: " << marks;
+    cout << "\nGrade: " << grade;
+    cout << "\nIs student: " << isStudent;
+    return 0;
+}
---
+++ 01_Basics/type_casting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 12;
+    int b = 15;
+    float sum = (float)a + b;
+    cout << "Sum = " << sum;
+    return 0;
+}
